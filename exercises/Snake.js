ba2e66dbfbaea2b34df730546e56e97bc9361051
Joel <joel.lopez.mendez@student.ehb.be>
2023-08-26 20:01:39 +0200

    textblokjes toegevoegd

--- exercises/Snake.js
+++ exercises/Snake.js
@@ -14,16 +14,16 @@ function callFunc() {
     arraySnakes[0].drawBack();
     arraySnakes[0].snake();
     arraySnakes[0].apple();
+    arraySnakes[0].text();
 }
 
 function setup() {
     let objectSnake = {
         xSnake: 0,
         ySnake: 300,
-        xApple: 125,
-        yApple: 330,
-        // xApple: Utils.randomNumber(0, width),
-        // yApple: Utils.randomNumber(0, height),
+        xApple: 125, // Utils.randomNumber(0, width),
+        yApple: 330, // Utils.randomNumber(0, height),
+
         drawBack: function () {
             context.fillStyle = "black";
             context.fillRect(0, 0, width, height);
@@ -31,7 +31,7 @@ function setup() {
                 for (let j = 0; j < height; j++) {
                     context.strokeStyle = "white";
                     Utils.drawLine(50 + i * 50, 0, 50 + i * 50, height);
-                    Utils.drawLine(0, 50 + i * 50, width, 50 + i * 50);
+                    Utils.drawLine(0, 0 + i * 50, width, 0 + i * 50);
                 }
             }
         },
@@ -49,6 +49,29 @@ function setup() {
         snake: function () {
             context.fillStyle = Utils.hsl(107, 100, 30);
             context.fillRect(0, this.ySnake, 50, 50);
+        },
+        score: 0,
+        amountBlocks: 50,
+        level: 1,
+        text: function () {
+            for (let i = 0; i < arraySnakes.length; i++) {
+                let objectSnake = arraySnakes[i];
+
+                context.fillStyle = "#5d5d5d";
+                context.fillRect(0, 1, 149, 49);
+                context.fillRect(201, 1, 148, 49);
+                context.fillRect(451, 1, 148, 49);
+                //Text info
+                context.fillStyle = "white";
+                context.font = "italic 24pt Arial";
+
+                //score
+                context.fillText("Score: " + objectSnake.score, 5, 45);
+                //Amount blocks
+                context.fillText(objectSnake.amountBlocks + " /50", 230, 45);
+                //Level
+                context.fillText("Level: " + objectSnake.level, 460, 45);
+            }
         }
     };
     arraySnakes.push(objectSnake);
